75520521a35c025a08df154a5455532394b7fe16
Liam Pieri <lpieri@redhat.com>
2018-11-05 15:57:40 -0500

    Add example playbook

--- library/hashicorp_vault.py
+++ library/hashicorp_vault.py
@@ -8,6 +8,25 @@ from ansible.module_utils.basic import *
     The purpose of this module is to make calls to the REST api of hashicorp
     vault
 
+- hosts: localhost
+  gather_facts: no
+  tasks:
+  - include_role:
+      name: hashicorp_vault
+    static: true  
+  - hashicorp_vault:
+      key: "foo"
+      value: "bar"
+      present: false
+      role_id:
+      secret_id: 
+      mount:  
+    register: secret_status 
+  - debug:
+      msg: "{{ item }}"
+    with_items:
+    - "{{secret_status.results}}"
+
 '''
 
 ANSIBLE_HASHI_VAULT_ADDR = 'https://vault.devshift.net/v1'
@@ -20,7 +39,7 @@ def delete_secret(fields):
         'X-Vault-token': fields['token'],
     }
     
-    api_url = "/".join([ANSIBLE_HASHI_VAULT_ADDR, fields['mount'], 
+    api_url = '/'.join([ANSIBLE_HASHI_VAULT_ADDR, fields['mount'], 
         fields['key']])
 
     r = requests.delete(api_url, headers=headers)
@@ -32,9 +51,9 @@ def store_secret(fields):
         'X-Vault-token': fields['token'],
     }
     
-    data = {'value': fields['value']}
+    data = {'data':{'value': fields['value']}}
 
-    api_url = '/'.join([ANSIBLE_HASHI_VAULT_ADDR, fields['mount'], 
+    api_url = '/'.join([ANSIBLE_HASHI_VAULT_ADDR, fields['mount'], 'data',
         fields['key']])
 
     r = requests.post(api_url, headers=headers, data=json.dumps(data))
@@ -45,7 +64,7 @@ def get_secret(fields):
         'X-Vault-token': fields['token'],
     }
     
-    api_url = "/".join([ANSIBLE_HASHI_VAULT_ADDR, fields['mount'], 
+    api_url = '/'.join([ANSIBLE_HASHI_VAULT_ADDR, fields['mount'], 
         fields['key']])
 
     r = requests.get(api_url, headers=headers)
@@ -54,7 +73,7 @@ def get_secret(fields):
 
 
 def approle_login(login_data):
-    api_url = "/".join([ANSIBLE_HASHI_VAULT_ADDR, 'auth/approle/login'])
+    api_url = '/'.join([ANSIBLE_HASHI_VAULT_ADDR, 'auth/approle/login'])
     r = requests.post(api_url,  data=json.dumps(login_data))
     
     return json.loads(r._content)['auth']['client_token']
@@ -63,30 +82,42 @@ def approle_login(login_data):
 def main():
 
     fields = {
-        "key": {"default": True, "type": "str"},
-        "value": {"default": True, "type": "str"},
-        "present": {"default": True, "type": "bool"},
-        "role_id": {"default": True, "type": "str"},
-        "secret_id": {"default": True, "type": "str"},
-        "mount": {"default": True, "type": "str"}
+        'key': {'default': True, 'type': 'str'},
+        'value': {'default': True, 'type': 'str'},
+        'present': {'default': True, 'type': 'bool'},
+        'role_id': {'default': True, 'type': 'str'},
+        'secret_id': {'default': True, 'type': 'str'},
+        'mount': {'default': True, 'type': 'str'}
     }
 
     module = AnsibleModule(argument_spec=fields)
     
     login_data = {
-        "role_id": fields['role_id'],
-        "secret_id": fields['secret_id']
+        'role_id': module.params.get('role_id'),
+        'secret_id': module.params.get('secret_id')
     }
 
-    fields['token'] = approle_login(login_data)
-
-    if fields['present']:
-        value = get_secret(fields)
+    token = approle_login(login_data)
+    
+    if module.params.get('present'):
+        params = {
+            'token': token,
+            'key': module.params.get('key'),
+            'mount': module.params.get('mount')
+        }
+        value = get_secret(params)
         results = {'value':value}
     else:
-        store_secret(fields)
+        params = {
+            'token': token,
+            'key': module.params.get('key'),
+            'value': module.params.get('value'),
+            'mount': module.params.get('mount')
+        }
+        store_secret(params)
+        results = {'value': 'success'}
     
-    module.exit_json(changed=True, results)
+    module.exit_json(changed=True, results=results)
 
 
 if __name__ == '__main__':
