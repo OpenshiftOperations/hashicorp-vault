d2f12b9b8711281707931f998b60af979817474a
Liam Pieri <lpieri@redhat.com>
2018-11-12 11:49:31 -0500

    Update param mapping

--- library/hashicorp_vault.py
+++ library/hashicorp_vault.py
@@ -7,26 +7,42 @@ from ansible.module_utils.basic import json, AnsibleModule
 ''' hashicorp_vault module
     The purpose of this module is to make calls to the REST api of hashicorp
     vault
-
+---
+---
 - hosts: localhost
   gather_facts: no
+  vars:
+    hashicorp_vault_role_id: 
+    hashicorp_vault_secret_id: 
+    hashicorp_vault_addr: 
+
   tasks:
-  - import_role:
-      name: hashicorp_vault
-  - hashicorp_vault:
-      key: "foo"
-      value: "bar"
-      role_id: ""
-      secret_id: ""
-      mount: ""
-    register: secret_status 
-  - debug:
-      msg: "{{ item }}"
-    with_items:
-    - "{{secret_status.results}}"
+    - name: Run the hashicorp_vault role
+      import_role:
+        name: liamwazherealso.openshift_hashicorp_vault
+    - name: Check secrets
+      hashicorp_vault:
+        role_id: "{{ hashicorp_vault_role_id }}"
+        secret_id: "{{ hashicorp_vault_secret_id }}"
+        vault_addr: "{{ hashicorp_vault_addr }}"
+        mount: 
+        name: 
+        data: 
+          key: value
+        
+      register: secret_status
+
+    - name: View secret results
+      debug:
+        msg: "{{ item }}"
+      loop:
+        - "{{secret_status.results}}"
+      
 
 '''
 
+class SecretNotFoundError(Exception):
+    """Exception for when a secret is not found"""
 
 def delete_secret(fields):
     """
@@ -50,22 +66,36 @@ def store_secret(fields):
     api_url = '/'.join([fields['vault_addr'], fields['mount'], 'data',
                         fields['name']])
     
-    data = {'data':fields['data']}
+    data = {'data': fields['data']}
+
+    try:
+        data.update(get_secret(fields)['data'])
+    except SecretNotFoundError:
+        pass
+
     r = requests.post(api_url, headers=headers, 
-                      data=json.dumps(data))
-
+                    data=json.dumps(data))
     return r
+    
 
+    
+
+    # Secret exists so update only new data
 
 def get_secret(fields):
     headers = {
         'X-Vault-token': fields['token'],
     }
-    
+
     api_url = '/'.join([fields['vault_addr'], fields['mount'], 'data',
                        fields['name']])
 
+
     r = requests.get(api_url, headers=headers)
+
+    if r.status_code == 404:
+        raise SecretNotFoundError()
+
 
     return json.loads(r._content)['data']
 
@@ -100,18 +130,21 @@ def main():
 
     token = approle_login(login_data)
     
-    if 'data' in module.params:
+    data = module.params.get('data')
+
+    if data:
         params = {
             'vault_addr': vault_addr,
             'token': token,
             'mount': module.params.get('mount'),
-            'name': module.params.get('name')
+            'name': module.params.get('name'),
+            'data': data
         }
         store_secret(params)
         results = {'value': 'success'}
     else:
         params = {
-            'vault': vault_addr,
+            'vault_addr': vault_addr,
             'token': token,
             'mount': module.params.get('mount'),
             'name': module.params.get('name')
